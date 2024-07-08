0a7c72d667c8e790266e1136b60f62eb81653a92
sarii0213 <anjing.singka@gmail.com>
2024-07-08 17:19:39 +0900

    :construction: ディレクトリ構成模索中
    Pages Router -> App Router

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
+"use client";
 import React from "react";
 import useSWR from "swr";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export default function App() {
+export default function Home() {
     const { data, error } = useSWR(
         "http://localhost:8080/api/v1/hello",
         fetcher
