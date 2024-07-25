34922c3399e8766fee715577312579e3bcb99931
sarii0213 <anjing.singka@gmail.com>
2024-07-25 12:12:03 +0900

    :recycle: spec, route, custom hook戻り値変数名のリファクタ

--- backend/spec/requests/users_apis_spec.rb
+++ backend/spec/requests/users_apis_spec.rb
@@ -2,12 +2,14 @@
 
 require 'rails_helper'
 
-describe 'GET /users/:id' do
-  it 'ユーザー設定の取得' do
-    user = create(:user)
-    get api_v1_user_path(user)
+RSpec.describe 'Api::V1::Users' do
+  describe 'GET /users/:id' do
+    it 'ユーザー設定の取得' do
+      user = create(:user)
+      get api_v1_user_path(user)
 
-    expect(response).to have_http_status(:ok)
-    expect(response.body).to include(user.username)
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include(user.username)
+    end
   end
 end
--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -3,21 +3,21 @@ import React from "react";
 import { useGetUser } from "@/hooks/useGetUser";
 
 const Settings = () => {
-  const { data, error, isLoading } = useGetUser();
+  const { data: userData, error, isLoading } = useGetUser();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
   return (
     <div>
-      {data && (
+      {userData && (
         <div>
           <h2 className="text-xl p-3">ユーザー設定</h2>
           <div className="">
             <ul>
               <li>ログイン情報: 🚧</li>
-              <li>目標体重: {data.goalWeight} kg</li>
-              <li>身長: {data.height} cm</li>
+              <li>目標体重: {userData.goalWeight} kg</li>
+              <li>身長: {userData.height} cm</li>
               <li>生理期間: 🚧</li>
             </ul>
           </div>
@@ -25,8 +25,8 @@ const Settings = () => {
           <h2 className="text-xl p-3 mt-6">表示設定</h2>
           <div className="">
             <ul>
-              <li>体脂肪率 表示: {data.displayBodyFat ? "ON" : "OFF"}</li>
-              <li>生理期間 表示: {data.displayPeriod ? "ON" : "OFF"}</li>
+              <li>体脂肪率 表示: {userData.displayBodyFat ? "ON" : "OFF"}</li>
+              <li>生理期間 表示: {userData.displayPeriod ? "ON" : "OFF"}</li>
             </ul>
           </div>
         </div>
